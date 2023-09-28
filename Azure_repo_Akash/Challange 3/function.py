9dc264879c12e34c6813ea15be7629bff32d0e5f
akash707x <146110411+akash707x@users.noreply.github.com>
2023-09-28 21:27:03 +0530

    Update function.py

--- Azure_repo_Akash/Challange 3/function.py
+++ Azure_repo_Akash/Challange 3/function.py
@@ -1,9 +1,8 @@
-# We have a nested object, we would like a function that you pass in the object and a key and get back the value. How this is implemented is up to you.
-##
-# Example Inputs
+# We have a nested object, we would like a function that can pass in the object and a key and get back the value. 
+
 # object = {“a”:{“b”:{“c”:”d”}}}
 # key = a/b/c
-##
+
 # object = {“x”:{“y”:{“z”:”a”}}}
 # key = x/y/z
 # value = a
